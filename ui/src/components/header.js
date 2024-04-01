bb5685385c590e960db5b336e63509c84574088f
Karthik-Katuri <kkaturi2003@gmail.com>
2024-04-01 16:13:32 +0530

    First commit 1/4/2024

---
+++ ui/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "./menu";
+
+const Header = () => {
+    return(
+        <header>
+            <h3>
+                Example project
+            </h3>
+            <Menu/>
+        </header>
+    )
+}
+export default Header;
---
+++ ui/src/components/menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu =()=>{
+    return(
+        <menu>
+            <li>Home</li>
+            <li>about</li>
+            <li>contact</li>
+        </menu>
+
+    );
+}
+
+export default Menu;
